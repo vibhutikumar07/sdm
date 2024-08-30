908aab7fdec6a96087cff566ef9097d7aeb4b8d0
Rishi Kunnath <82925475+rishikunnath2747@users.noreply.github.com>
2024-08-30 10:25:02 +0530

    Removing repo ID

--- sdm/src/main/java/com/sap/cds/sdm/constants/SDMConstants.java
+++ sdm/src/main/java/com/sap/cds/sdm/constants/SDMConstants.java
@@ -1,7 +1,7 @@
 package com.sap.cds.sdm.constants;
 
 public class SDMConstants {
-     public static final String REPOSITORY_ID = "b490f13f-f02e-4ef4-9fde-dc7e9dfda59a"; //System.getenv("REPOSITORY_ID");
+     public static final String REPOSITORY_ID = "repoid"; //System.getenv("REPOSITORY_ID");
      private static final String BEARER_TOKEN = "Bearer ";
      public static final String GRANT_TYPE = "grant_type";
      public static final String TENANT_ID = "X-zid";
